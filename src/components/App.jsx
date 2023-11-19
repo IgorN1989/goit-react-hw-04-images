b50705629a93af4dc17b3d306362d961cc7cd206
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-11-19 11:30:49 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 
 import { Container } from './Container/Container.styled';
@@ -9,87 +9,77 @@ import { Button } from './Button/Button';
 
 import { fetchImages } from 'api/pixabay-service';
 
-export class App extends Component {
-  state = {
-    galleryItems: [],
-    query: '',
-    page: 1,
-    loading: false,
-    error: false,
-    totalHits: 0,
-  };
+export const App = () => {
+  const [galleryItems, setGalleryItems] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [totalHits, setTotalHits] = useState(0);
 
-  componentDidUpdate(prevProps, prevState) {
-    const { query, page } = this.state;
+  useEffect(() => {
+    async function loadImages() {
+      try {
+        setLoading(true);
+        setError(false);
+        const data = await fetchImages(query, page);
+        const totalHits = data.total;
+        const images = data.hits;
 
-    if (prevState.query !== query || prevState.page !== page) {
-      this.loadImages();
-    }
-  }
+        setGalleryItems(prevState => [...prevState, ...images]);
+        setTotalHits(totalHits);
 
-  loadImages = async () => {
-    const { query, page } = this.state;
-    try {
-      this.setState({ loading: true, error: false });
-      const data = await fetchImages(query, page);
-      const totalHits = data.total;
-      const images = data.hits;
+        if (totalHits === 0) {
+          toast('Sorry, there are no matching images. Please try again.');
+          return;
+        }
 
-      this.setState(prevState => ({
-        galleryItems: [...prevState.galleryItems, ...images],
-        totalHits,
-      }));
-
-      if (totalHits === 0) {
-        toast('Sorry, there are no matching images. Please try again.');
-        return;
+        if (page === 1) {
+          toast.success(`Hooray! We found ${totalHits} images!`);
+        }
+      } catch (error) {
+        setError(true);
+        toast.error('Please reload this page');
+      } finally {
+        setLoading(false);
       }
-
-      if (page === 1) {
-        toast.success(`Hooray! We found ${totalHits} images!`);
-      }
-    } catch (error) {
-      this.setState({ error: true });
-      toast.error('Please reload this page');
-    } finally {
-      this.setState({ loading: false });
     }
-  };
 
-  submitSearch = newQuery => {
+    if (query === '') {
+      return;
+    }
+    loadImages();
+  }, [query, page]);
+
+  const submitSearch = newQuery => {
     if (!newQuery.trim()) {
       toast.error('Please, enter your search query.');
       return;
     }
 
-    if (newQuery === this.state.query) {
+    if (newQuery === query) {
       return;
     }
 
-    this.setState({
-      galleryItems: [],
-      query: newQuery,
-      page: 1,
-    });
+    setGalleryItems([]);
+    setQuery(newQuery);
+    setPage(1);
   };
 
-  increasePage = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const increasePage = () => {
+    setPage(prevState => prevState + 1);
   };
 
-  render() {
-    const { galleryItems, loading, error, totalHits } = this.state;
-    return (
-      <Container>
-        <Searchbar onSubmitSearch={this.submitSearch} />
-        {galleryItems.length > 0 && <ImageGallery images={galleryItems} />}
-        {loading && <Loader />}
-        {error}
-        {galleryItems.length > 0 && galleryItems.length < totalHits && (
-          <Button onLoadMoreBtn={this.increasePage} text="Load more" />
-        )}
-        <Toaster position="top-right" />
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <Searchbar onSubmitSearch={submitSearch} />
+      {galleryItems.length > 0 && <ImageGallery images={galleryItems} />}
+      {loading && <Loader />}
+      {error}
+      {galleryItems.length > 0 && galleryItems.length < totalHits && (
+        <Button onLoadMoreBtn={increasePage} text="Load more" />
+      )}
+      <Toaster position="top-right" />
+    </Container>
+  );
+};
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,36 +1,30 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { ModalComponent } from 'components/Modal/Modal';
 import { ImageContainer, Image } from './ImageGalleryItem.styled';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    isModalOpen: false,
+export const ImageGalleryItem = ({
+  image: { webformatURL, largeImageURL, tags },
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
   };
 
-  openModal = () => {
-    this.setState({ isModalOpen: true });
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
-  closeModal = () => {
-    this.setState({ isModalOpen: false });
-  };
+  return (
+    <ImageContainer>
+      <Image src={webformatURL} alt={tags} onClick={openModal} />
 
-  render() {
-    const { isModalOpen } = this.state;
-    const {
-      image: { webformatURL, largeImageURL, tags },
-    } = this.props;
-    return (
-      <ImageContainer>
-        <Image src={webformatURL} alt={tags} onClick={this.openModal} />
-
-        <ModalComponent
-          isOpen={isModalOpen}
-          onCloseModal={this.closeModal}
-          image={largeImageURL}
-          alt={tags}
-        />
-      </ImageContainer>
-    );
-  }
-}
+      <ModalComponent
+        isOpen={isModalOpen}
+        onCloseModal={closeModal}
+        image={largeImageURL}
+        alt={tags}
+      />
+    </ImageContainer>
+  );
+};
